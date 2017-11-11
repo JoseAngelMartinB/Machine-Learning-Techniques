972e50e288699d7d46b413014b4a4328c3ab8965
José Ángel Martín <joseangelmartinb@gmail.com>
2017-11-11 02:40:05 +0100

    Add San Juan features hierarchical clustering.

--- Activity_5/San_Juan.py
+++ Activity_5/San_Juan.py
@@ -10,9 +10,12 @@ López-Sepúlveda
 import sys
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
-from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import sklearn
+
+from sklearn import preprocessing, decomposition
+from scipy import cluster
+
 
 ### 0. Load the data asigned
 try:
@@ -24,21 +27,22 @@ try:
     data = data.loc[data['city'] == 'sj']
 
     data = data.fillna(0)
-    data = data.drop(['city','year','week_start_date'], 1)
+    data_ini = data.drop(['city','year','weekofyear','week_start_date','total_cases'], 1)
 
 except:
     print("Error while loading the data.")
     sys.exit()
 
 
+
 ### 1. Identify outliers
 
 # Normalization of the data
 min_max_scaler = preprocessing.MinMaxScaler()
-cases = min_max_scaler.fit_transform(data)
+cases = min_max_scaler.fit_transform(data_ini)
 
 # PCA Estimation
-estimator = PCA(n_components = 2)
+estimator = decomposition.PCA(n_components = 2)
 X_pca = estimator.fit_transform(cases)
 print("Percentage of variance explained by each of the selected components:")
 print(estimator.explained_variance_ratio_)
@@ -48,8 +52,8 @@ numbers = np.arange(len(X_pca))
 fig, ax = plt.subplots()
 for i in range(len(X_pca)):
     plt.text(X_pca[i][0], X_pca[i][1], numbers[i] + 2) # Because element index is element - 2
-plt.xlim(-0.75, 2)
-plt.ylim(-0.5, 3.5)
+plt.xlim(-0.5, 3.5)
+plt.ylim(-1, 1.5)
 ax.grid(True)
 fig.tight_layout()
 plt.show()
@@ -57,9 +61,29 @@ plt.show()
 # Remove outliers
 outliers = [89, 141, 401, 453, 713, 765]
 data = data.drop([x - 2 for x in outliers]) # Because element index is element - 2
+data_ini = data_ini.drop([x - 2 for x in outliers]) # Because element index is element - 2
+
 
 
 ### 2. Hierarchical clustering algorithm using features as elements
+
+# Normalization of the data
+norm_data_ini_features = min_max_scaler.fit_transform(np.transpose(data_ini))
+
+# Compute the similarity matrix
+dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
+matsim_ini_features = dist.pairwise(norm_data_ini_features)
+
+# Building the Dendrogram
+cut_threshold = 9
+features_label_lst = list(data_ini.columns.values)
+features_label = np.asarray(features_label_lst)
+
+clusters_features = cluster.hierarchy.linkage(matsim_ini_features, method = 'complete')
+cluster.hierarchy.dendrogram(clusters_features, color_threshold = cut_threshold, labels = features_label, leaf_rotation=90)
+plt.subplots_adjust(top=0.95, bottom=0.45)
+plt.show()
+
 
 
 ### 3. Correlation between features and total cases
